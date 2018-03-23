cbee843e159b2d9d6a29710a4d6da639f4d2a680
ah-med <ahmedomosanya@gmail.com>
2018-03-23 01:38:51 +0100

    Create user signin endpoint

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 import users from '../dummy_data/users';
+import checkUser from '../helpers/check';
 
 /**
  * @class UserModel
@@ -22,6 +23,15 @@ class UserModel {
 		// save the user
 		users.push(newUser);
 		return users[(users.length) - 1].id;
+	}
+	/**
+ 	* login a registerd user
+ 	*@param {*} email The request object.
+ 	*@param {*} password The request object.
+ 	*@returns {*} The return object
+ 	*/
+	static logUser(email, password) {
+		return checkUser(email, password, users);
 	}
 }
 
--- test/functions/helpers.js
+++ test/functions/helpers.js
@@ -1,1 +1,25 @@
-//will contain test for functions
+import chai from 'chai';
+import 'chai/register-should';
+import checkUser from '../../server/helpers/check';
+
+
+const { expect } = chai;
+
+describe('validate user in database', () => {
+	it('returns false for invalid users', () => {
+		const email = 'm@mail.com', password = 'dii';
+		const db = [
+			{
+				email: 'm@mail.com',
+				password: 'dii'
+			}
+		];
+		const valid = checkUser(email, password, db);
+		expect(valid).to.equal(true);
+	});
+
+	it('returns true for valid users', () => {
+		const invalid = checkUser();
+		expect(invalid).to.equal(false);
+	});
+});
